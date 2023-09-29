d2e37d9bc5dc45fd4aaad61506f7d81a4fde928b
Lyon <lyonleon93@gmail.com>
2023-09-29 18:47:39 +0200

    Fix typo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -97,13 +97,13 @@
 				<h1
 					class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl"
 				>
-					Real State Svelte
+					Real Estate Svelte
 					<span class="underline underline-offset-2 decoration-8 decoration-purple-400"
 						>Template</span
 					>
 				</h1>
 				<p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
-					Real State Svelte Template is a free, open source template made by <a
+					Real Estate Svelte Template is a free, open source template made by <a
 						class="text-purple-400"
 						href="https://x.com/melu1s"><strong>@melu1s</strong></a
 					>. Feel free to use it for your next project.
