9bf1b0d6d6185928044881768f3bad2b4dccdae3
Lyon <lyonleon93@gmail.com>
2023-09-30 17:40:38 +0200

    Refactor layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
 	import '../app.postcss';
+	import Footer from '$lib/components/Footer.svelte';
+	import Header from '$lib/components/Header.svelte';
 </script>
 
-<slot />
+<Header />
+<main>
+	<slot />
+</main>
+<Footer />
