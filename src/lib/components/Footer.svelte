080eddf9af97a9a57f53af35f8b1ce8d2f765a84
Lyon <lyonleon93@gmail.com>
2023-09-30 17:39:38 +0200

    Add components

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,129 @@
+<footer class="bg-[#001246] ">
+	<div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+		<div class="md:flex md:justify-between">
+			<div class="mb-6 md:mb-0">
+				<span class="text-white font-bold text-3xl">Logo</span>
+
+				<ul class="text-gray-400 font-medium mt-3 space-y-2">
+					<li class="flex items-center gap-1">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="15"
+							height="14"
+							viewBox="0 0 15 14"
+							fill="none"
+						>
+							<path
+								d="M7.49998 1.16669C5.04998 1.16669 2.83331 3.04502 2.83331 5.95002C2.83331 7.80502 4.26248 9.98669 7.11498 12.5009C7.33665 12.6934 7.66915 12.6934 7.89081 12.5009C10.7375 9.98669 12.1666 7.80502 12.1666 5.95002C12.1666 3.04502 9.94998 1.16669 7.49998 1.16669ZM7.49998 7.00002C6.85831 7.00002 6.33331 6.47502 6.33331 5.83335C6.33331 5.19169 6.85831 4.66669 7.49998 4.66669C8.14165 4.66669 8.66665 5.19169 8.66665 5.83335C8.66665 6.47502 8.14165 7.00002 7.49998 7.00002Z"
+								fill="#BDBDBD"
+							/>
+						</svg>
+						<p class="text">Street Name, City Name</p>
+					</li>
+					<li class="flex items-center gap-1">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="15"
+							height="14"
+							viewBox="0 0 15 14"
+							fill="none"
+						>
+							<path
+								d="M4.36167 6.29417C5.20167 7.945 6.555 9.2925 8.20583 10.1383L9.48917 8.855C9.64667 8.6975 9.88 8.645 10.0842 8.715C10.7375 8.93083 11.4433 9.0475 12.1667 9.0475C12.4875 9.0475 12.75 9.31 12.75 9.63083V11.6667C12.75 11.9875 12.4875 12.25 12.1667 12.25C6.68917 12.25 2.25 7.81083 2.25 2.33333C2.25 2.0125 2.5125 1.75 2.83333 1.75H4.875C5.19583 1.75 5.45833 2.0125 5.45833 2.33333C5.45833 3.0625 5.575 3.7625 5.79083 4.41583C5.855 4.62 5.80833 4.8475 5.645 5.01083L4.36167 6.29417Z"
+								fill="#BDBDBD"
+							/>
+						</svg>
+						<a href="tel:9051290512">+9051290512</a>
+					</li>
+					<li class="flex items-center gap-1">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="15"
+							height="14"
+							viewBox="0 0 15 14"
+							fill="none"
+						>
+							<path
+								d="M12.1667 2.33334H2.83335C2.19169 2.33334 1.67252 2.85834 1.67252 3.50001L1.66669 10.5C1.66669 11.1417 2.19169 11.6667 2.83335 11.6667H12.1667C12.8084 11.6667 13.3334 11.1417 13.3334 10.5V3.50001C13.3334 2.85834 12.8084 2.33334 12.1667 2.33334ZM12.1667 4.66668L7.50002 7.58334L2.83335 4.66668V3.50001L7.50002 6.41668L12.1667 3.50001V4.66668Z"
+								fill="#BDBDBD"
+							/>
+						</svg>
+						<a href="mailto:contact@company.com">contact@company.com</a>
+					</li>
+				</ul>
+			</div>
+
+			<div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-2">
+				<div>
+					<h2 class="mb-6 text-sm font-semibold uppercase text-white">Legal</h2>
+					<ul class=" text-gray-400 font-medium">
+						<li class="mb-4">
+							<a href="#" class="hover:underline">Privacy Policy</a>
+						</li>
+						<li>
+							<a href="#" class="hover:underline">Terms &amp; Conditions</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<hr class="my-6 sm:mx-auto border-gray-700 lg:my-8" />
+		<div class="sm:flex sm:items-center sm:justify-between">
+			<span class="text-sm sm:text-center text-gray-400"
+				>© 2023 <a href="#" class="hover:underline">Company Name™</a>. All Rights Reserved.
+			</span>
+			<div class="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
+				<a href="#" class="text-gray-500 hover:text-white">
+					<svg
+						class="w-4 h-4"
+						aria-hidden="true"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="currentColor"
+						viewBox="0 0 8 19"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					<span class="sr-only">Facebook page</span>
+				</a>
+
+				<a href="#" class="text-gray-500 hover:text-white">
+					<svg
+						class="w-4 h-4"
+						aria-hidden="true"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="currentColor"
+						viewBox="0 0 20 17"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					<span class="sr-only">Twitter page</span>
+				</a>
+
+				<a href="#" class="text-gray-500 hover:text-white">
+					<svg
+						class="w-4 h-4"
+						aria-hidden="true"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M10 0a10 10 0 1 0 10 10A10.009 10.009 0 0 0 10 0Zm6.613 4.614a8.523 8.523 0 0 1 1.93 5.32 20.094 20.094 0 0 0-5.949-.274c-.059-.149-.122-.292-.184-.441a23.879 23.879 0 0 0-.566-1.239 11.41 11.41 0 0 0 4.769-3.366ZM8 1.707a8.821 8.821 0 0 1 2-.238 8.5 8.5 0 0 1 5.664 2.152 9.608 9.608 0 0 1-4.476 3.087A45.758 45.758 0 0 0 8 1.707ZM1.642 8.262a8.57 8.57 0 0 1 4.73-5.981A53.998 53.998 0 0 1 9.54 7.222a32.078 32.078 0 0 1-7.9 1.04h.002Zm2.01 7.46a8.51 8.51 0 0 1-2.2-5.707v-.262a31.64 31.64 0 0 0 8.777-1.219c.243.477.477.964.692 1.449-.114.032-.227.067-.336.1a13.569 13.569 0 0 0-6.942 5.636l.009.003ZM10 18.556a8.508 8.508 0 0 1-5.243-1.8 11.717 11.717 0 0 1 6.7-5.332.509.509 0 0 1 .055-.02 35.65 35.65 0 0 1 1.819 6.476 8.476 8.476 0 0 1-3.331.676Zm4.772-1.462A37.232 37.232 0 0 0 13.113 11a12.513 12.513 0 0 1 5.321.364 8.56 8.56 0 0 1-3.66 5.73h-.002Z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					<span class="sr-only">Dribbble account</span>
+				</a>
+			</div>
+		</div>
+	</div>
+</footer>
---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,84 @@
+<header class="mb-12">
+	<nav class="bg-gray-100 py-3 fixed w-full z-20 top-0 left-0">
+		<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+			<a href="/" class="flex items-center">
+				<span class="self-center text-2xl font-semibold whitespace-nowrap">Logo</span>
+			</a>
+			<div class="flex md:order-2">
+				<button
+					type="button"
+					class="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg md:text-lg px-6 py-2 text-center mr-3 md:mr-0"
+					>Login</button
+				>
+				<button
+					data-collapse-toggle="navbar-sticky"
+					type="button"
+					class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+					aria-controls="navbar-sticky"
+					aria-expanded="false"
+				>
+					<span class="sr-only">Open main menu</span>
+					<svg
+						class="w-5 h-5"
+						aria-hidden="true"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 17 14"
+					>
+						<path
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M1 1h15M1 7h15M1 13h15"
+						/>
+					</svg>
+				</button>
+			</div>
+			<div
+				class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+				id="navbar-sticky"
+			>
+				<ul
+					class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0"
+				>
+					<li>
+						<a
+							href="/"
+							class="block py-2 pl-3 pr-4 text-white bg-purple-700 rounded md:bg-transparent md:text-purple-700 md:p-0"
+							aria-current="page">Home</a
+						>
+					</li>
+					<li>
+						<a
+							href="#property"
+							class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-700 md:p-0"
+							>Property</a
+						>
+					</li>
+					<li>
+						<a
+							href="#"
+							class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-700 md:p-0"
+							>About</a
+						>
+					</li>
+					<li>
+						<a
+							href="#service"
+							class=" block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-700 md:p-0"
+							>Service</a
+						>
+					</li>
+					<li>
+						<a
+							href="#contact"
+							class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-700 md:p-0"
+							>Contact</a
+						>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</header>
